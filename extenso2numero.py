4b21f09ff20bad8926a743cf6a41c97aec66721f
Clarissa <clarissa.david@semantix.com.br>
2019-04-17 11:23:32 -0300

    add documentation in code

--- extenso2numero.py
+++ extenso2numero.py
@@ -1,9 +1,23 @@
 # Author: Clarissa David
+
+## Importacao de bibliotecas
 import re
 import enchant
 from enchant.checker import SpellChecker
 
 def spellcorrect(string_extenso):
+    """
+    Descricao: funcao responsavel por corricao ortegrafica
+    Input:
+        arg1:
+        tipo: str
+        representa: string com numerico por extenso
+
+    Output:
+        out1:
+        tipo: str
+        representa: string com o numerico por extenso correto
+    """
     checker_spell = enchant.checker.SpellChecker("pt_BR")
     checker_spell.set_text(string_extenso)
     for err in checker_spell:
@@ -12,6 +26,34 @@ def spellcorrect(string_extenso):
     return checker_spell.get_text()
 
 def converter(string_extenso, numDict, milharDict, resultado, grupo):
+    """
+    Descricao: funcao responsavel por converter string por extenso para numerico
+    Input:
+        arg1:
+        tipo: str
+        representa: string por com numero por extenso
+        ----
+        arg2:
+        tipo: dict
+        representa: dicionario com os numeros dezenas e centezas do alfabeto pt-BR
+        ----
+        arg3:
+        tipo: dict
+        representa: dicionario com os numeros milhares do alfabeto pt-BR
+        ----
+        arg4:
+        tipo: num
+        representa: numerico para adicionar o resultado
+        ----
+        arg5:
+        tipo: num
+        representa: numerico para adicionar o grupo ao qual o numerico esta
+
+    Output:
+        out1:
+        tipo: num
+        representa: numerico resultante da conversao da string por extenso
+    """
     for word in string_extenso.split():            
         if(word in numDict):
             grupo += numDict[word]
@@ -22,6 +64,34 @@ def converter(string_extenso, numDict, milharDict, resultado, grupo):
     return resultado
 
 def currencyconverter(string_extenso, numDict, milharDict, resultado, grupo):
+    """
+    Descricao: funcao responsavel por converter string por extenso para monetario
+    Input:
+        arg1:
+        tipo: str
+        representa: string por com valor monetario por extenso
+        ----
+        arg2:
+        tipo: dict
+        representa: dicionario com os numeros dezenas e centezas do alfabeto pt-BR
+        ----
+        arg3:
+        tipo: dict
+        representa: dicionario com os numeros milhares do alfabeto pt-BR
+        ----
+        arg4:
+        tipo: num
+        representa: numerico para adicionar o resultado
+        ----
+        arg5:
+        tipo: num
+        representa: numerico para adicionar o grupo ao qual o numerico esta
+
+    Output:
+        out1:
+        tipo: num
+        representa: numerico resultante da conversao da string por extenso
+    """
     #string_numerico = spellcorrect(string_extenso)
     if("reais" not in string_extenso):
         string_extenso = "zero reais e " + string_extenso
